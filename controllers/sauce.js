63ad2403bc68846c298f478058b3e392a676812a
Ashoory <ashooryr@amazon.com>
2021-04-18 17:56:24 -0400

    first commit

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -56,26 +56,28 @@ exports.modifySauce = (req, res, next) => {
         const url = req.protocol + '://' + req.get('host');
         req.body.sauce = JSON.parse(req.body.sauce);
       sauce = {
-            _id: req.params.id,  
-            userId: req.body.sauce.userId,
-            name: req.body.sauce.name,
-            manufacturer: req.body.sauce.manufacturer,
-            description: req.body.sauce.description,
-            mainPepper: req.body.sauce.mainPepper,
-            imageUrl: url + '/images/' + req.file.filename,
-            heat: req.body.sauce.heat,
-         };
+    _id: req.params.id,  
+    userId: req.body.sauce.userId,
+    name: req.body.sauce.name,
+    manufacturer: req.body.sauce.manufacturer,
+    description: req.body.sauce.description,
+    mainPepper: req.body.sauce.mainPepper,
+    imageUrl: url + '/images/' + req.file.filename,
+    heat: req.body.sauce.heat,
+    
+   };
 } else {
-      sauce = {  
-            _id: req.params.id,  
-            userId: req.body.userId,
-            name: req.body.name,
-            manufacturer: req.body.manufacturer,
-            description: req.body.description,
-            mainPepper: req.body.mainPepper,
-            //imageUrl:  req.body.imageUrl,
-            heat: req.body.heat,
-      };
+  sauce = {  
+    _id: req.params.id,  
+    userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    //imageUrl:  req.body.imageUrl,
+    heat: req.body.heat,
+    
+  };
 }
   Sauce.updateOne({_id: req.params.id}, sauce).then(
     () => {
@@ -129,35 +131,36 @@ exports.getAllSauce = (req, res, next) => {
   );
 };
 
+
 /////// Dislike & Like Function  Export///////
+
 
 
 exports.likeOrDislikeSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id }).then(
     (sauce) => {
       let updateObject = {}
-     
       if(req.body.like==1 && sauce.usersLiked.indexOf(req.body.userId)<0) {
-        sauce.usersLiked.push(req.body.userId);
-        sauce.likes+=1;
+        updateObject.usersLiked = sauce.usersLiked.push(req.body.userId);
+        updateObject.likes = sauce.likes+=1;
       } else if (req.body.like==-1 && sauce.usersDisliked.indexOf(req.body.userId)<0) {
-        sauce.usersDisliked.push(req.body.userId);
-        sauce.dislikes+=1;
+       updateObject.usersDisliked =  sauce.usersDisliked.push(req.body.userId);
+       updateObject.dislikes =  sauce.dislikes+=1;
       } else {
         sauce.usersLiked.forEach(element => {
           if(element==req.body.userId) {
-            sauce.likes-=1;
-            sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId),1);
+          updateObject.likes =  sauce.likes-=1;
+          updateObject.usersLiked =  sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId),1);
           }
         });
         sauce.usersDisliked.forEach(element => {
           if(element==req.body.userId) {
-            sauce.dislikes-=1;
-            sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(req.body.userId),1);
+            updateObject.dislikes =  sauce.dislikes-=1;
+            updateObject.usersDisliked =  sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(req.body.userId),1);
           }
         });
       }
-      
+      console.log(updateObject);
       Sauce.updateOne({ _id: req.params.id }, updateObject).then(
         () => {
           res.status(201).json({
@@ -174,3 +177,4 @@ exports.likeOrDislikeSauce = (req, res, next) => {
     }
   )
 }
+
